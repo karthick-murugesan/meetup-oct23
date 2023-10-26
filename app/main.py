b48abcc41f6f7511b89569c9b200689a0f1b8293
karthick-murugesan <karthick.murugesan@saama.com>
2023-10-26 21:53:22 +0530

    latest changes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
 from models.post import UserPost
+from fastapi.responses import JSONResponse
+
 
 app=FastAPI()
+headers = {'Access-Control-Allow-Origin': '*',
+        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*'
+        }
 
 @app.get("/health")
 async def hello():
-    return {"message":"hello world"}
+    res={"message":"hello world"}
+    return JSONResponse(res, status_code=200, headers=headers)
 
 @app.post("/savedetails")
 async def savedetails(post:UserPost):
-    return post.name + post.gender
+    # print(post)
+    return JSONResponse(post, status_code=200, headers=headers)
+        
+        
+       
+
--- app/models/post.py
+++ app/models/post.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
 
 class UserPost(BaseModel):
-    name:str
-    gender:str
-    
+    first_name:str
+    last_name:str
+    College:str
+    email:str
+    contact:str
+    radio_set1:str
+    radio_set2:str
+    radio_set3:str
+    radio_set4:str
+    radio_set5:str
+    about_the_event:str
+    Future_meetup_topics:str
+    feedback:str
