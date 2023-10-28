4bf2c471009d6443c6ca53677d28c01e635d9f7e
Karthick <104968648+karthick-murugesan@users.noreply.github.com>
2023-10-28 10:11:51 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
 
 @app.get("/health")
 async def hello():
-    res={"message":"Hello swashikan"}
+    res={"message":"Kaalai vanakkam"}
     return JSONResponse(res, status_code=200, headers=headers)
 
 
